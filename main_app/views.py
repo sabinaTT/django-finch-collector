fbdd025787ddc1c56358cda0361c3bdcd3c05039
sabinaTT <tairsbn@gmail.com>
2022-04-06 21:59:28 -0700

    refactor Home.html to extend base

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View # View class to handle requests
-from django.views import HttpResponse # a class to handle sending a type of response
+from django.http import HttpResponse # a class to handle sending a type of response
 from django.views.generic.base import TemplateView
 
 # Create your views here.
