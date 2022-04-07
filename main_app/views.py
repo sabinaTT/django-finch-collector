92cf855c5aa4417532d34623434aa422e0abb3e5
sabinaTT <tairsbn@gmail.com>
2022-04-06 21:49:53 -0700

    update the view function to use the template

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.views import View # View class to handle requests
 from django.views import HttpResponse # a class to handle sending a type of response
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
 # Here we will be creating a class called Home and extending it from the View class
-class Home(View):
-
+class Home(TemplateView):
+    template_name = "home.html"
     # Here we are adding a method that will be run when we are dealing with a GET request
-    def get(self, request):
+    # def get(self, request):
         # Here we are returning a generic response
         # This is similar to response.send() in express
-        return HttpResponse("Finch Home")
+        # return HttpResponse("Finch Home")
